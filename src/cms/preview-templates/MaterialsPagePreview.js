fe4d96531c64d8855bf756d881de9fad69d1f3ab
Sean Newell <sean@thenewells.us>
2020-05-24 21:07:02 -0500

    Fixup import

--- src/cms/preview-templates/MaterialsPagePreview.js
+++ src/cms/preview-templates/MaterialsPagePreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import '../assets/styles.sass'
+import '../../assets/styles.sass'
 import { MaterialPageTemplate } from '../../templates/materials-page'
 
 const MaterialsPagePreview = ({ entry, getAsset }) => {
