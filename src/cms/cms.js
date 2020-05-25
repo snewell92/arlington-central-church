a16a66c518663262a9ee1771030b68bb274cb372
Sean Newell <sean@thenewells.us>
2020-05-24 22:15:26 -0500

    Adjust materials preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,4 @@
 import CMS from 'netlify-cms'
-import '../styles/global.sass'
 
 // import EventsPagePreview from './preview-templates/EventsPagePreview'
 import MaterialsPagePreview from './preview-templates/MaterialsPagePreview'
--- src/cms/preview-templates/MaterialsPagePreview.js
+++ src/cms/preview-templates/MaterialsPagePreview.js
@@ -9,11 +9,10 @@ const MaterialsPagePreview = ({ entry, getAsset }) => {
   return (
     <MaterialPageTemplate
       title={entry.getIn(['data', 'title'])}
+      subtitle={entry.getIn(['data', 'subtitle'])}
       meta_title={entry.getIn(['data', 'meta_title'])}
       meta_description={entry.getIn(['data', 'meta_description'])}
-      heading={entry.getIn(['data', 'heading'])}
-      description={entry.getIn(['data', 'description'])}
-      files={links}
+      links={links}
     />
   )
 }
