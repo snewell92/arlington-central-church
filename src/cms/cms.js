79192cf7ad963ecd13ce6e54d2cde5bddeae9be7
Sean Newell <sean@thenewells.us>
2020-05-24 23:23:02 -0500

    Use proper cms module

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,5 @@
-import CMS from 'netlify-cms'
-
+import CMS from 'netlify-cms-app'
 import '../components/all.sass'
-
-// import EventsPagePreview from './preview-templates/EventsPagePreview'
 import MaterialsPagePreview from './preview-templates/MaterialsPagePreview'
 
-// CMS.registerPreviewTemplate('events', EventsPagePreview)
 CMS.registerPreviewTemplate('materials', MaterialsPagePreview)
