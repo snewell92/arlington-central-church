81357a618f3c04e72abcacbd081b3e2d0d83e0a0
Sean Newell <sean@thenewells.us>
2020-05-24 22:34:03 -0500

    Move styles around into layout

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <h3 class="subtitle">
+  <footer className="footer">
+    <div className="content has-text-centered">
+      <h3 className="subtitle">
         The Arlington Central Church
       </h3>
       <p><a href="/admin">Admin Page</a></p>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import '../all.sass';
 import config from '../../../meta/config'
 import NavBar from '../NavBar'
 import Footer from '../Footer'
