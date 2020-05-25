8046d0ea874edd5f1b8ebfe4e68f2160c8ae938a
Sean Newell <sean@thenewells.us>
2020-05-24 22:22:50 -0500

    Adjust favicon

--- src/html.js
+++ src/html.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import favicon from './img/favicon.ico'
+import appleIcon from './img/apple-touch-icon.png'
+import favMd from './img/favicon-32x32.png'
+import favLg from './img/favicon-16x16.png'
 
 export default ({ headComponents, body, postBodyComponents }) => {
   return (
@@ -9,19 +12,9 @@ export default ({ headComponents, body, postBodyComponents }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
         {headComponents}
         <link rel='shortcut icon' href={favicon} />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href={appleIcon} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favLg} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favMd} />
       </head>
       <body>
         <div
