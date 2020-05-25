2952e5a4a38a09c726fdc80359b13fa690c0a3d3
Sean Newell <sean@thenewells.us>
2020-05-24 21:32:45 -0500

    Try importing global style in root cms js file

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
 import CMS from 'netlify-cms'
+import '../styles/global.sass'
 
 // import EventsPagePreview from './preview-templates/EventsPagePreview'
 import MaterialsPagePreview from './preview-templates/MaterialsPagePreview'
